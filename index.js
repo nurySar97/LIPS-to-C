6d69941d3761b58cc7aeab0e1d6e0410c192a597
Muhammet <nsaryyev9797@gmail.com>
2021-07-18 21:53:12 +0300

    Transformer was created

--- index.js
+++ index.js
@@ -202,15 +202,75 @@ function traverser(ast, visitor) {
     traverseNode(ast, null);
 }
 
+// TRANSFORMER
+function transformer(ast) {
+
+    let newAst = {
+        type: 'Program',
+        body: [],
+    };
+
+    ast._context = newAst.body;
+
+    traverser(ast, {
+
+        NumberLiteral: {
+            enter(node, parent) {
+                parent._context.push({
+                    type: 'NumberLiteral',
+                    value: node.value,
+                });
+            },
+        },
+
+        StringLiteral: {
+            enter(node, parent) {
+                parent._context.push({
+                    type: 'StringLiteral',
+                    value: node.value,
+                });
+            },
+        },
+
+        CallExpression: {
+            enter(node, parent) {
+                let expression = {
+                    type: 'CallExpression',
+                    callee: {
+                        type: 'Identifier',
+                        name: node.name,
+                    },
+                    arguments: [],
+                };
+
+                node._context = expression.arguments;
+
+                if (parent.type !== 'CallExpression') {
+                    expression = {
+                        type: 'ExpressionStatement',
+                        expression: expression,
+                    };
+                }
+
+                parent._context.push(expression);
+            }
+        }
+    });
+
+    return newAst;
+}
+
 
 
 let tokens = tokenizer('(add 2 (subtract 4 2))');
 let ast = parser(tokens);
-
+let newAST = transformer(ast);
 
 log(red, '(add 2 (subtract 4 2))');
 
 log(blue, tokens);
 
 log(green, ast);
+
+log(cyan, newAST);
 
